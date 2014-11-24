0d9d4651a3cdd81b39d62ced18785ece2bcf35cd
aav <aav@marketlogicsoftware.com>
2014-11-24 22:34:29 +0100

    Add models

---
+++ aag_oauth_service/__init__.py
@@ -0,0 +1,15 @@
+__author__ = 'aurel'
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_oauthlib.provider import OAuth2Provider
+
+app = Flask(__name__)
+db = SQLAlchemy()
+oauth = OAuth2Provider()
+
+
+def init_app(settings='aag_oauth_service.oauth.config'):
+    app.config.from_object(settings)
+    db.init_app(app)
+    oauth.init_app(app)
--- aag_oauth_service/models.py
+++ aag_oauth_service/models.py
@@ -1,6 +1,6 @@
 __author__ = 'aurel'
 
-from aag.oauth import db
+from aag_oauth_service.oauth import db
 
 
 class Client(db.Model):
