b65dd6de024e0acfaa707cefc40dbaf226eca3ae
Aurel Avramescu <aav@marketlogicsoftware.com>
2014-11-24 17:31:55 +0100

    Create new branch

--- aag/oauth/server.py
+++ aag/oauth/server.py
@@ -1,45 +1,15 @@
 __author__ = 'aurel'
 
-from tornado.web import RequestHandler
-from oauth2.web import Request, SiteAdapter
-import oauth2
+import tornado.ioloop
+import tornado.web
 
 
-class OAuth2Handler(RequestHandler):
-    """
-    Dispatches requests to an authorization controller
-    """
-    def initialize(self, controller):
-        self.controller = controller
-
+class OauthHandler(tornado.web.RequestHandler):
     def get(self):
-        response = self._dispatch_request()
-        self._map_response(response)
+        self.write("Hello, world")
 
-    def post(self):
-        response = self._dispatch_request()
-        self._map_response(response)
-
-    def _dispatch_request(self):
-        request = Request(request_handler=self)
-
-        return self.controller.dispatch(request, environ={})
-
-    def _map_response(self, response):
-        for name, value in list(response.headers.items()):
-            self.set_header(name, value)
-
-        self.set_status(response.status_code)
-        self.write(response.body)
-
-
-class WebSiteAdapter(SiteAdapter):
-    def authenticate(self, request, environ, scopes):
-        return {}
-
-
-def main():
-    pass
+application = tornado.web.Application([(r"/authorize", OauthHandler)])
 
 if __name__ == "__main__":
-    main()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
--- aag/oauth/test/tests_server.py
+++ aag/oauth/test/tests_server.py
@@ -1,11 +1,4 @@
 __author__ = 'aurel'
 
-import unittest
-from unittest.mock import MagicMock
-from aag.oauth.server import OAuth2Handler
 
 
-class OAuth2HandlerTest(unittest.TestCase):
-    def setUp(self):
-        #request_handler =
-        handler = OAuth2Handler()
